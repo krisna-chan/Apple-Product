f77adb88a2adff27c2987654d3f587c6acaa4895
krisna <chankrisnayun@gmail.com>
2024-04-08 21:00:00 +0700

    new file

--- dist/custom.18d3a4a7.js
+++ dist/custom.18d3a4a7.js
@@ -172,7 +172,7 @@ module.exports = reloadCSS;
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../font/SanFranciscoFont-master/SanFranciscoDisplay-Regular.otf":[["SanFranciscoDisplay-Regular.b5dc36f2.otf","../font/SanFranciscoFont-master/SanFranciscoDisplay-Regular.otf"],"../font/SanFranciscoFont-master/SanFranciscoDisplay-Regular.otf"],"_css_loader":"../../../../../opt/homebrew/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../../opt/homebrew/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../font/SanFranciscoFont-master/SanFranciscoDisplay-Regular.otf":[["SanFranciscoDisplay-Regular.b5dc36f2.otf","../font/SanFranciscoFont-master/SanFranciscoDisplay-Regular.otf"],"../font/SanFranciscoFont-master/SanFranciscoDisplay-Regular.otf"],"./../assets/hero_iphone15pro__i70z9oz3hj2i_small.jpg":[["hero_iphone15pro__i70z9oz3hj2i_small.036bc19a.jpg","../assets/hero_iphone15pro__i70z9oz3hj2i_small.jpg"],"../assets/hero_iphone15pro__i70z9oz3hj2i_small.jpg"],"_css_loader":"../../../../../opt/homebrew/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../../opt/homebrew/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64328" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56419" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
