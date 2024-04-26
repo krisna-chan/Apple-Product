2e9974250e6df7f72b5c46f11162a067561fe70e
Yun Chankrisna <chankrisnayun@gmail.com>
2024-04-26 20:05:50 +0700

    card section 26.04.

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -6941,7 +6941,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49235" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63436" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
