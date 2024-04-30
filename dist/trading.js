9ba782fe3c01435cc37c63119b4905446c2f4302
Yun Chankrisna <chankrisnayun@gmail.com>
2024-04-30 16:17:22 +0700

    init: appleclone krisna coolest

--- dist/trading.js
+++ dist/trading.js
@@ -193,7 +193,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55122" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65245" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
@@ -338,4 +338,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../../../opt/homebrew/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/index.js.map
+//# sourceMappingURL=/trading.js.map
