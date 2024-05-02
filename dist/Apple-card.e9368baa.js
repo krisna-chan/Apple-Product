7328911b6092749ea4734ac6b03d67a9d7119d1c
Yun Chankrisna <chankrisnayun@gmail.com>
2024-05-02 19:17:37 +0700

    page : trading page , apple-card

--- dist/Apple-card.e9368baa.js
+++ dist/Apple-card.e9368baa.js
@@ -121,7 +121,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../card_page/large_pic/built_for_iphone__d59h2lf8xtiu_large.jpg":[["built_for_iphone__d59h2lf8xtiu_large.21ead275.jpg","../card_page/large_pic/built_for_iphone__d59h2lf8xtiu_large.jpg"],"../card_page/large_pic/built_for_iphone__d59h2lf8xtiu_large.jpg"],"_css_loader":"../../../../../opt/homebrew/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../../opt/homebrew/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../card_page/large_pic/photo-1614851099362-9adf73ccebe9.avif":[["photo-1614851099362-9adf73ccebe9.8adc9368.avif","../card_page/large_pic/photo-1614851099362-9adf73ccebe9.avif"],"../card_page/large_pic/photo-1614851099362-9adf73ccebe9.avif"],"_css_loader":"../../../../../opt/homebrew/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../../opt/homebrew/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -146,7 +146,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55122" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50931" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import * as bootstrap from 'bootstrap';
+import { eventListeners } from '@popperjs/core';
+// import * as bootstrap from 'bootstrap';
+
+// const Apple = document.getElementById("iphone")
+// const Watch  = document.getElementById("iwatch")
+
+// eventListeners('click' , Apple)=>{
+//     console.log('hello')
+// }
